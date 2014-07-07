7f036fb2deeca3f5efaca1d19750518bc37fbf42
David Su <dtscraft@gmail.com>
2014-07-07 00:37:22 -0700

    new: html attrs as hamlpy attrs. Smart class/ids
    - html attributes now rendered as hamlpy attributes
    - test_class_with_dot_and_hash
    - test_id_with_dot_and_hash

--- lib/html2hamlpy/html.py
+++ lib/html2hamlpy/html.py
@@ -3,10 +3,26 @@ from bs4 import Tag, CData, Comment, Tag, NavigableString, Doctype, BeautifulSou
 import re
 def to_haml_soup(self):
     return ''.join(child.to_haml(tabs=0) for child in (self.children or []) )
-def to_haml_tag(self, tabs):
+
+def to_haml_tag(self, tabs, **kwargs):
     output = tabulate(tabs)
+    kwargs['instance'] = self
+
+    if (not
+        ((self.name == 'div') and
+        (
+            ('id' in self.attrs and is_static_id(**kwargs)) or
+
+            (
+                ('class' in self.attrs and is_static_classname(**kwargs)) and
+                ('class' in self.attrs and any(map(lambda c: is_haml_css_attr(c), self.attrs['class'])))
+            )
+        ))
+    ):
+        output +=  ('%%%s' % self.name)
+
     if len(self.attrs) > 0:
-        if 'id' in self.attrs and is_static_id(self.attrs['id']):
+        if 'id' in self.attrs and is_static_id(**kwargs):
             output +=  ("#%s" % self.attrs['id'])
             del self.attrs['id']
         if 'class' in self.attrs:
@@ -15,6 +31,7 @@ def to_haml_tag(self, tabs):
             leftover = filter(lambda c: not is_haml_css_attr(c), self.attrs['class'])
             del self.attrs['class']
             if any(leftover) : self.attrs['class'] = ' '.join(leftover)
+    if len(self.attrs) > 0: output += haml_attributes(**kwargs)
     return output + ''.join(child.to_haml(tabs=0) for child in (self.children or []) )
 def to_haml_cdata(self, tabs):
     #TODO
@@ -28,19 +45,36 @@ def to_haml_comment(self, tabs):
 def to_haml_doctype(self, tabs):
     #TODO
     pass
-def is_static_id(id, **kwargs):
-    #TODO
-    return is_haml_css_attr(id)
+
+def is_static_id(**kwargs):
+    instance = kwargs['instance']
+    return  is_static_attribute('id', **kwargs) and is_haml_css_attr(instance.attrs['id'])
+
 def is_haml_css_attr(attr):
     return bool(re.match(r'^[-:\w]+$', attr))
+
 def is_dynamic_attribute(name, **kwargs):
     #TODO
-    return false
-def is_static_attribute(options):
-    pass
-def is_static_classname(name, **kwargs):
+    return False
+
+def is_static_attribute(name, **kwargs):
+    instance = kwargs['instance']
+    return name in instance.attrs and not is_dynamic_attribute(name, **kwargs)
+
+def is_static_classname(**kwargs):
+    return is_static_attribute('class', **kwargs)
+
+def is_dynamic_attribute(name, **kwargs):
     #TODO
-    pass
+    return False
+
+def haml_attributes(**kwargs):
+    instance = kwargs['instance']
+    attrs = map(lambda (name, value): haml_attribute_pair(name, value), instance.attrs.items())
+    return "{%s}" % ', '.join(attrs)
+
+def haml_attribute_pair(name, value, **kwargs):
+    return "%s:\"%s\"" % (name, value)
 
 def tabulate(tabs):
     return '  ' * tabs
--- tests/test_html2hamlpy.py
+++ tests/test_html2hamlpy.py
@@ -9,5 +9,14 @@ class HtmlToHamlPyTest(unittest.TestCase):
     def test_multiple_class_names(self):
         self.assertEqual('.foo.bar.baz', render('<div class=" foo bar baz "></div>'))
 
+    def test_class_with_dot_and_hash(self):
+        self.assertEqual('%div{class:"foo.bar"}', render("<div class='foo.bar'></div>"))
+        self.assertEqual('%div{class:"foo#bar"}', render("<div class='foo#bar'></div>"))
+        self.assertEqual('.foo.bar{class:"foo#bar foo.bar"}', render("<div class='foo foo#bar bar foo.bar'></div>"))
+
+    def test_id_with_dot_and_hash(self):
+        self.assertEqual('%div{id:"foo.bar"}', render("<div id='foo.bar'></div>"))
+        self.assertEqual('%div{id:"foo#bar"}', render("<div id='foo#bar'></div>"))
+
 if __name__ == '__main__':
     unittest.main()
