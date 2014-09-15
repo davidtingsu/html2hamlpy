b0b9d186908063028756d702700107681f0aa63c
David Su <dtscraft@gmail.com>
2014-09-14 23:46:25 -0700

    add license and setuptools configuration
    - add MIT license: http://choosealicense.com/licenses/mit/
    - add setuptools configuration

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+    name = "html2hamlpy",
+    version = "0.11.3",
+    packages = find_packages(exclude=["*.tests", "*.tests.*"]),
+
+    # Project uses beautifulsoup4, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['beautifulsoup4==4.3.2'],
+
+    package_data = {
+                # If any package contains *.txt, *.md files, include them:
+                '': ['*.txt','*.md'],
+    },
+
+    # metadata for upload to PyPI
+    author = "David Su",
+    author_email = "dtscraft@gmail.com",
+    description = "Convert Django-flavored HTML to HamlPy",
+    long_description = open("README.md").read(),
+    license = "MIT",
+    keywords = "haml hamlpy django converter html2hamlpy",
+    url = "http://github.com/davidtingsu/html2hamlpy",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
